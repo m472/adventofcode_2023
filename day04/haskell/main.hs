fc723f174909dbbffc376549571dc361d6a55eb3
Mathias Graf <mathias.n.graf@gmail.com>
2023-12-04 23:36:37 +0100

    part 2 of day 4 works now

--- day04/haskell/main.hs
+++ day04/haskell/main.hs
@@ -20,11 +20,21 @@ parseCard line = (read (words card !! 1) :: Int, map read $ words winning, map r
 countWinningNumbers :: Card -> Int
 countWinningNumbers (_, winning, actual) = length (actual `intersect` winning)
 
+partOne :: [Card] -> IO ()
+partOne cards = print $ sum $ map ((`div` 2) . (2 ^) . countWinningNumbers) cards
+
+partTwo :: [Card] -> IO ()
+partTwo cards = print $ partTwo' wins (replicate (length wins) 1)
+  where
+    wins = map countWinningNumbers cards
+
+partTwo' :: [Int] -> [Int] -> Int
+partTwo' [] [] = 0
+partTwo' (x : xs) (y : ys) = y + partTwo' xs (zipWith (+) (replicate x y ++ repeat 0) ys)
+
 main = do
   content <- readFile "../input.txt"
   let cards = map parseCard $ lines content
 
-  -- part One
-  print $ sum $ map  ((`div` 2) . (2 ^ ) . countWinningNumbers) cards
-
-  print $ map countWinningNumbers cards
+  partOne cards
+  partTwo cards
