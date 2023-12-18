9db1ec39b3c75d4da26a9547d4605fb6a681f3b2
Mathias Graf <mathias.n.graf@gmail.com>
2023-12-18 21:52:59 +0100

    day 16 part 2 added (takes about 1m41s, could filter out already visited start points but I won't)

--- day16/haskell/main.hs
+++ day16/haskell/main.hs
@@ -1,23 +1,23 @@
 import Data.Array (Array, Ix (inRange), array, bounds, elems, indices, (!), (//))
-import Debug.Trace (trace, traceShowId)
+import Data.Set (Set, empty, insert, notMember)
 
-data Direction = North | South | East | West deriving (Eq, Show)
+data Direction = North | South | East | West deriving (Eq, Ord, Show)
 
 parseInput :: String -> Array (Int, Int) Char
 parseInput content =
   let inputMap = zipWith (\j line -> zipWith (\i c -> ((i, j), c)) [1 ..] line) [1 ..] (lines content)
    in array ((1, 1), (length inputMap, length (head inputMap))) (concat inputMap)
 
-simulate :: Array (Int, Int) Char -> Array (Int, Int) Bool
-simulate arr = simulate' [((1, 1), East)] [] (array (bounds arr) [(i, False) | i <- indices arr]) arr
+simulate :: ((Int, Int), Direction) -> Array (Int, Int) Char -> Array (Int, Int) Bool
+simulate start arr = simulate' [start] empty (array (bounds arr) [(i, False) | i <- indices arr]) arr
 
-simulate' :: [((Int, Int), Direction)] -> [((Int, Int), Direction)] -> Array (Int, Int) Bool -> Array (Int, Int) Char -> Array (Int, Int) Bool
+simulate' :: [((Int, Int), Direction)] -> Set ((Int, Int), Direction) -> Array (Int, Int) Bool -> Array (Int, Int) Char -> Array (Int, Int) Bool
 simulate' [] _ res arr = res
 simulate' ((pos, dir) : remainingNodes) visited res arr =
   let currentChar = arr ! pos
       newDir = changeDir dir currentChar
-      newNodeList = filter (inRange (bounds arr) . fst) $ map (\d -> (move pos d, d)) newDir
-   in simulate' (filter (`notElem` visited) (newNodeList ++ remainingNodes)) ((pos, dir) : visited) (res // [(pos, True)]) arr
+      newNodeList = filter (`notMember` visited) $ filter (inRange (bounds arr) . fst) $ map (\d -> (move pos d, d)) newDir
+   in simulate' (newNodeList ++ remainingNodes) (insert (pos, dir) visited) (res // [(pos, True)]) arr
 
 changeDir :: Direction -> Char -> [Direction]
 changeDir dir '.' = [dir]
@@ -49,8 +49,26 @@ disp f arr =
   let ((min_x, min_y), (max_x, max_y)) = bounds arr
    in putStrLn $ unlines [[f (arr ! (i, j)) | i <- [min_x .. max_x]] | j <- [min_y .. max_y]]
 
+countTrue :: Array (Int, Int) Bool -> Int
+countTrue = foldl (\total y -> total + if y then 1 else 0) 0
+
+partOne :: Array (Int, Int) Char -> Int
+partOne input = countTrue $ simulate ((1, 1), East) input
+
+partTwo :: Array (Int, Int) Char -> Int
+partTwo input =
+  let ((min_x, min_y), (max_x, max_y)) = bounds input
+   in maximum $
+        map
+          (countTrue . (`simulate` input))
+          ( [((i, min_y), South) | i <- [min_x .. max_x]]
+              ++ [((i, max_y), North) | i <- [min_x .. max_x]]
+              ++ [((min_x, j), East) | j <- [min_y .. max_y]]
+              ++ [((max_x, j), West) | j <- [min_y .. max_y]]
+          )
+
 main = do
   content <- readFile "../input.txt"
   let input = parseInput content
-  disp id input
-  print $ length $ filter id $ elems $ simulate input
+  print $ partOne input
+  print $ partTwo input
